87643f29c43c92d09456ef5cd7942b9cf8ef19bb
bobbyg603 <bobbyg603@gmail.com>
2023-07-04 21:47:43 +0000

    fix: plugins dir path

--- Source/BugSplat/Private/BugSplatSettings.cpp
+++ Source/BugSplat/Private/BugSplatSettings.cpp
@@ -8,9 +8,9 @@
 #include "BugSplatEditorSettings.h"
 #include "BugSplatRuntime.h"
 
-FBugSplatSettings::FBugSplatSettings(FString UProjectFilePath)
+FBugSplatSettings::FBugSplatSettings()
 {
-	FBugSplatSettings::UProjectFilePath = UProjectFilePath;
+
 }
 
 FString FBugSplatSettings::CreateBugSplatEndpointUrl()
@@ -54,33 +54,41 @@ void FBugSplatSettings::UpdateCrashReportClientIni(FString DefaultEngineIniFileP
 
 void FBugSplatSettings::WriteSymbolUploadScript()
 {
+	FString SetCurrentPlatfrom = FString("set targetPlatform=%1");
+	FString TargetPlatformNullGuard = FString("if \"%targetPlatform%\"==\"\" (\n\techo \"BugSplat [ERROR]: symbol upload invocation missing target platform...\"\n\texit /b\n)");
+	FString EditorPlatformGuard = FString("if \"%targetPlatform%\"==\"Editor\" (\n\techo \"BugSplat [INFO]: Editor build detected, skipping symbol uploads...\"\n\texit /b\n)");
+	UBugSplatEditorSettings* RuntimeSettings = FBugSplatRuntimeModule::Get().GetSettings();
+
 	FString PostBuildStepsConsoleCommandFormat =
 		FString(
-			"\"{0}\" "	 // Uploader Path
-			"-i {1} "	 // Client ID
-			"-s {2} "	 // Client Secret
-			"-b {3} "	 // Database
-			"-a {4} "	 // Application
-			"-v {5} "	 // Version
-			"-d \"{6}\" " // Project Directory
-			"-f \"{7}\" " // File Pattern
+			"{0}\n"		  // Set Platform
+			"{1}\n"		  // Target Platform Null Guard
+			"{2}\n"		  // Editor Platform Guard
+			"\"{3}\" "	  // Uploader Path
+			"-i {4} "	  // Client ID
+			"-s {5} "	  // Client Secret
+			"-b {6} "	  // Database
+			"-a {7} "	  // Application
+			"-v {8} "	  // Version
+			"-d \"{9}\" " // Project Directory
+			"-f \"{10}\" " // File Pattern
 		);
 
 	FStringFormatOrderedArguments args;
-
-	UBugSplatEditorSettings* runtimeSettings = FBugSplatRuntimeModule::Get().GetSettings();
-
+	args.Add(SetCurrentPlatfrom);
+	args.Add(TargetPlatformNullGuard);
+	args.Add(EditorPlatformGuard);
 	args.Add(BUGSPLAT_SYMBOL_UPLOADER_PATH);
-	args.Add(runtimeSettings->BugSplatClientId);
-	args.Add(runtimeSettings->BugSplatClientSecret);
-	args.Add(runtimeSettings->BugSplatDatabase);
-	args.Add(runtimeSettings->BugSplatApp);
-	args.Add(runtimeSettings->BugSplatVersion);
+	args.Add(RuntimeSettings->BugSplatClientId);
+	args.Add(RuntimeSettings->BugSplatClientSecret);
+	args.Add(RuntimeSettings->BugSplatDatabase);
+	args.Add(RuntimeSettings->BugSplatApp);
+	args.Add(RuntimeSettings->BugSplatVersion);
 	args.Add(FPaths::Combine(FPaths::ConvertRelativePathToFull(FPaths::ProjectDir()), "Binaries"));
 	args.Add("**/*.{pdb,dll,exe}");
-
-	FString FormattedString = *FString::Format(*PostBuildStepsConsoleCommandFormat, args);
-	FFileHelper::SaveStringToFile(FormattedString, *BUGSPLAT_BASH_DIR);
+	
+	FString UploadSymbolsScript = FString::Format(*PostBuildStepsConsoleCommandFormat, args);
+	FFileHelper::SaveStringToFile(UploadSymbolsScript, *BUGSPLAT_BASH_DIR);
 	FMessageDialog::Debugf(FText::FromString("Symbol uploads added successfully!"));
 }
 
