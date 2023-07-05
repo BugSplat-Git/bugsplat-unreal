c904ede19bcb99102d8481faccac0799af09e062
Bobby Galli <bobbyg603@gmail.com>
2023-07-05 16:42:19 -0400

    chore: fix symbol uploads for Win64 (#53)

--- Source/Scripts/upload-symbols-win64.bat
+++ Source/Scripts/upload-symbols-win64.bat
@@ -1,10 +1,12 @@
 echo off
 
 set targetPlatform=%1
-set pluginDir=%2
-set uploadScriptPath=%pluginDir%\BugSplat.bat
+set projectDir=%2
+set uploadScriptPath=%projectDir%\Plugins\BugSplat\Source\Scripts\BugSplat.bat
 
-IF NOT EXIST %uploadScriptPath% (
+echo "BugSplat [INFO]: invoking upload script at %uploadScriptPath%"
+
+if not exist "%uploadScriptPath%" (
 	echo "BugSplat [WARN]: symbol uploads not configured via plugin - skipping..."
 	exit /b
 )
