71f3f27e14fe48aa43414187d9d2ba2c15936658
Bobby Galli <bobbyg603@pm.me>
2025-03-03 22:51:08 -0500

    feat: linux uploads (WIP)

---
+++ Source/Scripts/setup-upload-symbols-linux.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Copyright (C) BugSplat. All Rights Reserved.
+
+export targetPlatform=$1
+export targetName=$2
+export projectPath=$3
+export pluginPath=$4
+
+export binariesPath=$projectPath/Binaries/Linux
+export configPath=$projectPath/Config
+export scriptsPath=$pluginPath/Source/Scripts
+
+export uploaderFolderPath=$pluginPath/Source/ThirdParty/SymUploader
+export uploaderPath=$uploaderFolderPath/symbol-upload-linux
+
+echo "BugSplat [INFO]: Input target platform: $targetPlatform" 
+echo "BugSplat [INFO]: Input target name: $targetName" 
+echo "BugSplat [INFO]: Input binaries path: $binariesPath"
+echo "BugSplat [INFO]: Input config path: $configPath"
+echo "BugSplat [INFO]: Input scripts path: $scriptsPath"
+echo "BugSplat [INFO]: Symbol uploader path: $uploaderPath"
+
+if [ "$targetPlatform" != "Linux" ]; then
+    echo "BugSplat [WARN]: Unexpected platform ${targetPlatform}, skipping symbol upload..." 
+    exit
+fi
+
+if [[ "$targetName" == *"Editor"* ]]; then
+    echo "BugSplat [INFO]: Editor build detected, skipping symbol upload..."
+    exit
+fi
+
+if [ ! -f "$uploaderPath" ]; then
+    echo "BugSplat [INFO]: File $uploaderPath does not exist - downloading..."
+    mkdir -p $uploaderFolderPath
+    curl -sL "https://app.bugsplat.com/download/symbol-upload-linux" -o $uploaderPath
+    chmod +x $uploaderPath
+fi
